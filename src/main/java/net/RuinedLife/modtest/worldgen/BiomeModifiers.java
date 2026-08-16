ba9ed8417dd1be10d556f4e99e86187e28832b0d
Ruined-life <ruinedlife.exe1@gmail.com>
2026-08-16 20:58:50 +1000

    Changes: Content: -Added scarlet trees to naturally generate in plains biomes now.
    Dev:

--- src/main/java/net/RuinedLife/modtest/worldgen/BiomeModifiers.java
+++ src/main/java/net/RuinedLife/modtest/worldgen/BiomeModifiers.java
@@ -8,6 +8,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,6 +17,8 @@ public class BiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_NEW_ZEAL_LAND_ORE = registerKey("add_new_zeal_land_ore");
     public static final ResourceKey<BiomeModifier> ADD_NETHER_NEW_ZEAL_LAND_ORE = registerKey("add_nether_new_zeal_land_ore");
     public static final ResourceKey<BiomeModifier> ADD_END_NEW_ZEAL_LAND_ORE = registerKey("add_end_new_zeal_land_ore");
+
+    public static final ResourceKey<BiomeModifier> ADD_SCARLET_TREE = registerKey("add_scarlet_tree");
 
 
     public static void bootstrap(BootstapContext<BiomeModifier> context) {
@@ -36,6 +39,11 @@ public class BiomeModifiers {
                 biomes.getOrThrow(BiomeTags.IS_END),
                 HolderSet.direct(placedFeatures.getOrThrow(PlacedFeatures.END_NEW_ZEAL_LAND_ORE_PLACED_KEY)),
                 GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_SCARLET_TREE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(Tags.Biomes.IS_PLAINS),
+                HolderSet.direct(placedFeatures.getOrThrow(PlacedFeatures.SCARLET_TREE_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION));
     }
 
     private static ResourceKey<BiomeModifier> registerKey(String name) {
--- src/main/java/net/RuinedLife/modtest/worldgen/PlacedFeatures.java
+++ src/main/java/net/RuinedLife/modtest/worldgen/PlacedFeatures.java
@@ -1,10 +1,13 @@
 package net.RuinedLife.modtest.worldgen;
 
 import net.RuinedLife.modtest.modtest;
+import net.RuinedLife.modtest.registries.BlockRegistry;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -20,6 +23,8 @@ public class PlacedFeatures {
     public static final ResourceKey<PlacedFeature> NETHER_NEW_ZEAL_LAND_ORE_PLACED_KEY = registerKey("nether_new_zeal_land_ore_placed");
     public static final ResourceKey<PlacedFeature> END_NEW_ZEAL_LAND_ORE_PLACED_KEY = registerKey("end_new_zeal_land_ore_placed");
 
+    public static final ResourceKey<PlacedFeature> SCARLET_TREE_PLACED_KEY = registerKey("scarlet_tree_placed");
+
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
@@ -32,6 +37,11 @@ public class PlacedFeatures {
         register(context, END_NEW_ZEAL_LAND_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ConfiguredFeatures.END_NEW_ZEAL_LAND_ORE_KEY),
                 OrePlacement.commonOrePlacement(12,
                         HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
+
+        register(context, SCARLET_TREE_PLACED_KEY, configuredFeatures.getOrThrow(ConfiguredFeatures.SCARLET_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(1, 0.1f, 1),
+                        BlockRegistry.SCARLET_SAPLING.get()));
+
     }
 
 
